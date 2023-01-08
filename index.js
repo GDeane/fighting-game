2c80d6cddf8268693e915acaa5accb3cb0a9d813
Gavin Deane <gavindeane1@gmail.com>
2023-01-08 12:26:14 -0800

    completed idle animation

--- index.js
+++ index.js
@@ -36,6 +36,24 @@ const player = new Fighter({
     }, offset: {
         x: 0,
         y: 0
+    },
+    imageSrc: './img/samuraiMack/Idle.png',
+    framesMax: 8,
+    scale: 2.5,
+    offset: {
+        x: 215,
+        y: 157
+    },
+    sprites: {
+        idle: {
+            imageSrc: './img/samuraiMack/Idle.png',
+            framesMax: 8
+        },
+        run: {
+            imageSrc: './img/samuraiMack/Run.png',
+            framesMax: 8,
+            image: new Image()
+        }
     }
 })
 
@@ -76,7 +94,7 @@ function animate() {
     background.update()
     shop.update()
     player.update()
-    enemy.update()
+    // enemy.update()
 
     player.velocity.x = 0
     enemy.velocity.x = 0
