0c4c7ae5a658157f2f8d262bd92242b335bb991b
Gavin Deane <gavindeane1@gmail.com>
2023-01-21 11:28:20 -0800

    Revert "completed idle animation" due to unknown change that broke the visuals
    This reverts commit 2c80d6cddf8268693e915acaa5accb3cb0a9d813.

--- js/classes.js
+++ js/classes.js
@@ -1,11 +1,5 @@
 class Sprite {
-    constructor({
-                    position,
-                    imageSrc,
-                    scale = 1,
-                    framesMax = 1,
-                    offset = {x: 0, y: 0}
-                }) {
+    constructor({position, imageSrc, scale = 1, framesMax = 1}) {
         this.position = position
         this.width = 50
         this.height = 150
@@ -16,7 +10,6 @@ class Sprite {
         this.framesCurrent = 0
         this.framesElapsed = 0
         this.framesHold = 11
-        this.offset = offset
     }
 
     draw() {
@@ -26,16 +19,16 @@ class Sprite {
             0,
             this.image.width / this.framesMax,
             this.image.height,
-            this.position.x - this.offset.x,
-            this.position.y - this.offset.y,
+            this.position.x,
+            this.position.y,
             (this.image.width / this.framesMax) * this.scale,
             this.image.height * this.scale
         )
     }
 
-    animateFrames() {
+    update() {
+        this.draw()
         this.framesElapsed++
-
         if (this.framesElapsed % this.framesHold === 0) {
             if (this.framesCurrent < this.framesMax - 1) {
                 this.framesCurrent++
@@ -44,32 +37,11 @@ class Sprite {
             }
         }
     }
-
-    update() {
-        this.draw()
-        this.animateFrames()
-    }
 }
 
-class Fighter extends Sprite {
-    constructor({
-                    position,
-                    velocity,
-                    color = 'red',
-                    imageSrc,
-                    scale = 1,
-                    framesMax = 1,
-                    offset = {x: 0, y: 0},
-                    sprites
-                }) {
-        super({
-            position,
-            imageSrc,
-            scale,
-            framesMax,
-            offset
-        })
-
+class Fighter {
+    constructor({position, velocity, color = 'red', offset}) {
+        this.position = position
         this.velocity = velocity
         this.width = 50
         this.height = 150
@@ -82,20 +54,21 @@ class Fighter extends Sprite {
         this.color = color
         this.isAttacking
         this.health = 100
-        this.framesCurrent = 0
-        this.framesElapsed = 0
-        this.framesHold = 11
-        this.sprites = sprites
+    }
 
-        for (sprite in sprites) {
+    draw() {
+        c.fillStyle = this.color
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
 
+        // attack box
+        if (this.isAttacking) {
+            c.fillStyle = 'green'
+            c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
         }
     }
 
     update() {
         this.draw()
-        this.animateFrames()
-
         this.attackBox.position.x = this.position.x + this.attackBox.offset.x
         this.attackBox.position.y = this.position.y
 
